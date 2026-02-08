42dea1f5767bf742119e63893469772d156f3c7e
Gunara07 <gulnara_suleymanova@mail.ru>
2026-02-09 00:57:27 +0400

    DZ 3

---
+++ src/main/java/com/example/userservice/dao/BaseDaoTest.java
@@ -0,0 +1,26 @@
+package com.example.userservice.dao;
+
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public abstract class BaseDaoTest {
+
+    @Container
+    protected static final PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:15-alpine")
+                    .withDatabaseName("testdb")
+                    .withUsername("test")
+                    .withPassword("test");
+
+    static {
+        postgres.start();
+
+        System.setProperty("hibernate.connection.url", postgres.getJdbcUrl());
+        System.setProperty("hibernate.connection.username", postgres.getUsername());
+        System.setProperty("hibernate.connection.password", postgres.getPassword());
+        System.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL95Dialect");
+        System.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+    }
+}
---
+++ src/main/java/com/example/userservice/entity/UsersTest.java
@@ -0,0 +1,44 @@
+package com.example.userservice.entity;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+
+public class UsersTest {
+
+    @Test
+    void testConstructorSetsCreatedAt() {
+        Users user = new Users("Vasya", "vasya@test.com", 25);
+
+        assertNotNull(user.getCreatedAt());
+        assertTrue(user.getCreatedAt().isBefore(LocalDateTime.now().plusSeconds(1)));
+    }
+
+    @Test
+    void testGettersAndSetters() {
+        Users user = new Users();
+
+        user.setId(1L);
+        user.setName("Vika");
+        user.setEmail("vika@test.com");
+        user.setAge(30);
+        LocalDateTime now = LocalDateTime.now();
+        user.setCreatedAt(now);
+
+        assertEquals(1L, user.getId());
+        assertEquals("Vika", user.getName());
+        assertEquals("vika@test.com", user.getEmail());
+        assertEquals(30, user.getAge());
+        assertEquals(now, user.getCreatedAt());
+    }
+
+    @Test
+    void testToString() {
+        Users user = new Users("Borya", "borya@test.com", 35);
+        user.setId(42L);
+
+        String expected = "Пользователь 42: Borya (borya@test.com), возраст: 35";
+        assertEquals(expected, user.toString());
+    }
+}
+
